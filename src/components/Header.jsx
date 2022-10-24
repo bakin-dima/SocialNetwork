be62dcbfc6ae0b2c137fdec31da39b5650587f26
Dima Bakin <dima@MacBook-Air-Dima.local>
2022-10-24 17:57:37 +0300

    feat: add components

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => (
+  <header className="header">
+    <img src="https://img.lovepik.com/element/45007/9994.png_300.png" alt=""></img>
+  </header>
+);
+
+export default Header;
