9cddffc3ebd66696b2e9f4f2e4353b5537bb9ee9
Bakin Dima <Bakin.dima93@gmail.com>
2023-01-21 20:56:34 +0300

    feat: 71 add local state for status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Preloader from "../../common/preloader/Preloader";
 import styles from "./ProfileInfo.module.css";
 import userPhoto from "../../../assets/images/user_avatar_placeholder.png";
+import ProfileStatus from "./ProfileStatus";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -10,15 +11,15 @@ const ProfileInfo = (props) => {
 
   return (
     <div className={styles.content}>
-      <div className={styles.profileImage}>
+      {/* <div className={styles.profileImage}>
         <img src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300" alt="userBackground" />
-      </div>
+      </div> */}
       <div className={styles.profileDescription}>
         <img className={styles.avatar} src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto} alt="avatar" />
 
-        <div>
+        <div className={styles.Description}>
           <div>{props.profile.fullName}</div>
-          <div>{props.profile.aboutMe}</div>
+          <ProfileStatus status={"My test status"} />
         </div>
       </div>
     </div>
